1380b9a7e4e67e8e59eac1856238c0bf8f9964d6
Tim Richardson <software@potton.me.uk>
2017-03-06 10:00:14 +0000

    Removed my own OpenWeather code

--- PiClock.py
+++ PiClock.py
@@ -75,7 +75,7 @@ logging.info('Starting by defining variables and dictionaries')
 CityLocation = 2639964  # Potton
 WeatherUnits = 'imperial'
 WeatherURLFormat = 'http://api.openweathermap.org/data/2.5/forecast?id={}&APPID={}'
-OpenWeatherMapKey = '7df1a4b48bacd8bfd001012c1c7878f0'
+OpenWeatherMapKey = 'yourweatherkeyhere'
 
 # -----------------------------------------------------------------------------
 # Images
