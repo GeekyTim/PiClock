55aa081119cd28441971d5261800c485fd383f67
Tim Richardson <software@potton.me.uk>
2020-11-01 18:45:56 +0000

    Update config_openweathermap.py

--- code/config_openweathermap.py
+++ code/config_openweathermap.py
@@ -4,6 +4,6 @@
 CityLocation = 2639964  # Potton
 WeatherUnits = 'imperial'
 WeatherURLFormat = 'http://api.openweathermap.org/data/2.5/forecast?id={}&APPID={}'
-OpenWeatherMapKey = '7df1a4b48bacd8bfd001012c1c7878f0'
+OpenWeatherMapKey = 'XXXXXX'
 SecondsBetweenWeatherRefresh = 3600
 SecondsBetweemWeatherRefreshOnError = 60
